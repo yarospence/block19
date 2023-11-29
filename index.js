711568a879d6265e1b52c9417621997f749f3252
Spencer Toliver <tolivercapital@gmail.com>
2023-11-29 10:38:26 -0600

    completed block19

---
+++ index.js
@@ -0,0 +1,13 @@
+const freelancers () => {
+   [
+  { name: "Dr. Slice", price: 25, occupation: "gardener" },
+  { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+  { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+  { name: "Prof. Prism", price: 81, occupation: "teacher" },
+  { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+  { name: "Prof. Spark", price: 76, occupation: "programmer" },
+  { name: "Dr. Wire", price: 47, occupation: "teacher" },
+  { name: "Prof. Goose", price: 72, occupation: "driver" },
+];
+}
+console.log(freelancers.name);
---
+++ readMe.md
@@ -0,0 +1,5 @@
+//Today we are creating a freelancer forum 
+
+It will include the name, price, and occupation of each freelancer
+
+We will have a function that calculates the average of the freelancers starting price
